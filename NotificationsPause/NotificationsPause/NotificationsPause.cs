f8aa66bf292f43fe4441a8bb73c0f26af4dd025a
Johannes Kunkel <johannes.kunkel@gmx.net>
2019-04-20 18:07:01 +0200

    Updated all the things

--- NotificationsPause/NotificationsPause/NotificationsPause.cs
+++ NotificationsPause/NotificationsPause/NotificationsPause.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
+using System.Reflection;
+using Newtonsoft;
+using Newtonsoft.Json;
 using Harmony;
 using UnityEngine;
 
@@ -9,15 +12,78 @@ namespace NotificationsPause
 {
     public static class NotificationsPause
     {
+        private static SettingsFile settings;
+        private static bool tryReadOnce = true;
+        private static float lastPause = 0f;
+        private class SettingsFile
+        {
+            public string fileversion;
+            public float cooldown;
+            public SortedDictionary<string, bool> PauseOnNotification;
+        }
+
         [HarmonyPatch(typeof(Notification), "IsReady")]
         public static class Notification_IsReady_Patch
         {
+            private static void readSettings()
+            {
+                FileInfo sfile = new FileInfo(Assembly.GetExecutingAssembly().Location);
+                DirectoryInfo dirInfo = sfile.Directory;
+                string settingpath = dirInfo.FullName + "/settings.json";
+
+                FileInfo filechecker = new FileInfo(settingpath);
+                if (!filechecker.Exists)
+                    return;
+
+                StreamReader sr = new StreamReader(settingpath);
+                string settstr=sr.ReadToEnd();
+                try {
+                    settings = Newtonsoft.Json.JsonConvert.DeserializeObject<SettingsFile>(settstr);
+                } catch (JsonReaderException exc)
+                {
+                    sr.Close();
+                    sr.Dispose();
+                    Debug.Log("Critical Notification Pauser: Error reading Json");
+                }
+                sr.Close();
+                sr.Dispose();
+            }
+
             public static void Postfix(ref Notification __instance)
             {
-                if (__instance.Type == NotificationType.Bad || __instance.Type == NotificationType.DuplicantThreatening)
+                if (tryReadOnce && settings == null)
                 {
-                    if (!(__instance.titleText=="Combat!"))
-                        SpeedControlScreen.Instance.Pause();
+                    //First notification, read file and stuff
+                    readSettings();
+                    tryReadOnce = false;
+                }
+
+                
+                if (settings != null && settings.PauseOnNotification != null && settings.PauseOnNotification.ContainsKey(__instance.titleText))
+                {
+                    if (Time.time - lastPause > settings.cooldown)
+                    {
+                        //If the title is set in the settings, use that
+                        if (settings.PauseOnNotification[__instance.titleText])
+                        {
+                            SpeedControlScreen.Instance.Pause();
+                            lastPause = Time.realtimeSinceStartup;
+                        }
+                    }
+                }
+                else {
+                    //... otherwise use default behaviour
+                    if (__instance.Type == NotificationType.Bad || __instance.Type == NotificationType.DuplicantThreatening)
+                    {
+                        if (!((__instance.titleText == "Combat!") || (__instance.titleText == "Missing Research Station")))
+                        {
+                            if (Time.time - lastPause > 1.0)
+                            {
+                                SpeedControlScreen.Instance.Pause();
+                                lastPause = Time.realtimeSinceStartup;
+                            }
+                        }
+                    }
                 }
             }
         }
